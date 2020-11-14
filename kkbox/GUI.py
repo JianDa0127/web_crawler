6cc1c7c3d0056296db9fd48bd0a4d0dcff3b02a5
Glow <fnckaliezer@gmail.com>
2020-11-15 00:03:44 +0800

    建立播放器
    1.實現播放器功能(播放, 暫停, 上一首歌, 下一首歌)
    2.現在音量條可以改變音量了
    3.可以調整播放模式(隨機, 單曲循環)
    4.部分快捷鍵可以使用了(空白鍵：暫停, control+Q 關閉視窗, ...)

--- kkbox/GUI.py
+++ kkbox/GUI.py
@@ -7,8 +7,6 @@ import sys
 sys.path.append('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/UI_Designer')
 sys.path.append('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/UI_Designer/icon')
 from main_screen import Ui_MainWindow
-import KKboxCrawler
-import datetime
 import unittest
 
 
@@ -19,25 +17,77 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         self.setupUi(self)
 
         # 播放器設置
-
-        # 播放器按鈕設置
-        # Icons made by <ahref="https://www.flaticon.com/authors/bqlqn"title = "bqlqn">bqlqn</a>from<ahref = "https://www.flaticon.com/"title = "Flaticon">www.flaticon.com</a>
-
-        # self.pushButton_play.clicked.connect(self.playMusic)
-        # self.pushButton_previous.clicked.connect(self.previousMusic)
-        # self.pushButton_next.clicked.connect(self.nextMusic)
-        # self.pushButton_random.clicked.connect(self.randomMusic)
-        # self.pushButton_repeat.clicked.connect(self.repeatMusic)
+        self.playlist = QMediaPlaylist(self)
+        self.player = QMediaPlayer(self)
+        self.media_content = QMediaContent(QUrl.fromLocalFile('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/text.mp3'))
+        # self.player.setMedia(QMediaContent(QUrl('http://example.com/music.mp3')))
+        self.player.setMedia(self.media_content)
+        ## 音量設置
+        self.volume_Slider.valueChanged.connect(self.volume_slider_func)
+        self.player.setVolume(50)
+        self.player.play()
+        self.duration = self.player.duration() #取得歌曲長度(macOS無法設定...)
 
         # 狀態列功能設置
         self.retranslateUi(self)
         self.actionClose.triggered.connect(app.exit)
+        # self.actionsOpen.triggered.connect(self.file_open)
+        self.actionPlay.triggered.connect(self.playMusic)
+        self.actionNext.triggered.connect(self.nextMusic)
+        self.actionPrevious.triggered.connect(self.previousMusic)
+        self.actionSuffle.triggered.connect(self.Music_mode)
+        self.actionRepeat.triggered.connect(self.Music_mode)
+        # self.actionVolume_Up.triggered.connect(self)
+        # self.actionVolume_Down.triggered.connect(self)
+
+        # 播放器按鈕設置
+        # Icons made by <ahref="https://www.flaticon.com/authors/bqlqn"title = "bqlqn">bqlqn</a>from<ahref = "https://www.flaticon.com/"title = "Flaticon">www.flaticon.com</a>
+
+        self.pushButton_play.clicked.connect(self.playMusic)
+        self.pushButton_previous.clicked.connect(self.previousMusic)
+        self.pushButton_next.clicked.connect(self.nextMusic)
+        self.pushButton_random.clicked.connect(self.Music_mode)
+        self.pushButton_repeat.clicked.connect(self.Music_mode)
 
         # 取得comboBox_place, comboBox_release, comboBox_lang選取的值
         self.pushButton_OK.clicked.connect(self.get_comboBoxValue)
 
-    # 地區設置
+    # def file_open(self):
+    #     # 開啟檔案
+    def playMusic(self):
+        # 播放音樂
+        if self.player.state() == 1:
+            self.player.pause()
+        else:
+            self.player.play()
+    def nextMusic(self):
+        # 下一首歌
+        if self.playlist.currentIndex() == self.playlist.mediaCount() - 1:
+            self.playlist.setCurrentIndex(0)
+        else:
+            self.playlist.next()
+    def previousMusic(self):
+        # 上一首歌
+            if self.playlist.currentIndex() == 0:
+                self.playlist.setCurrentIndex(self.playlist.mediaCount() - 1)
+            else:
+                self.playlist.previous()
+    def Music_mode(self):
+        # 音樂模式
+        if self.playlist.playbackMode() == 2:
+            self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+
+        elif self.playlist.playbackMode() == 3:
+            self.playlist.setPlaybackMode(QMediaPlaylist.Random)
+
+        elif self.playlist.playbackMode() == 4:
+            self.playlist.setPlaybackMode(QMediaPlaylist.Sequential)
+    def volume_slider_func(self, value):
+        # 音量條
+        self.player.setVolume(value)
+
     def get_comboBoxValue(self):
+        # 取得地區,歌曲類型,歌曲語言
         place_select_value = self.comboBox_place.currentText()
         release_select_value = self.comboBox_release.currentText()
         lang_select_value = self.comboBox_lang.currentText()
@@ -45,9 +95,6 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         print(place_select_value)
         print(release_select_value)
         print(lang_select_value)
-    # 播放器按鈕功能設置
-    # def playMusic(self):
-
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)

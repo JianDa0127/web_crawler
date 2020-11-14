3964850b6526d0aa10161ad8280d222d6b8e05a2
Glow <fnckaliezer@gmail.com>
2020-11-14 13:17:01 +0800

    刻一個播放器造型出來
    1.還沒有實現功能

--- kkbox/GUI.py
+++ kkbox/GUI.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import sys
 sys.path.append('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/UI_Designer')
 from main_screen import Ui_MainWindow
@@ -13,16 +15,18 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         # 主界面初始化
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
         # 狀態列功能設置
         self.retranslateUi(self)
         self.actionClose.triggered.connect(app.exit)
-        # 取得comboBox_place, comboBox_release, comboBox_lang的值
-        self.pushButton01.clicked.connect(self.get_comboBoxValue)
+        # 取得comboBox_place, comboBox_release, comboBox_lang選取的值
+        self.pushButton_OK.clicked.connect(self.get_comboBoxValue)
     # 地區設置
     def get_comboBoxValue(self):
         place_select_value = self.comboBox_place.currentText()
         release_select_value = self.comboBox_release.currentText()
         lang_select_value = self.comboBox_lang.currentText()
+
         print(place_select_value)
         print(release_select_value)
         print(lang_select_value)

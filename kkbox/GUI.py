0fd12b3f256b75ce8a85071f245bea2c3495b911
Glow <fnckaliezer@gmail.com>
2020-11-14 17:40:25 +0800

    介面更新
    1.改變主介面icon
    2.新增播放列表
    3.新增播放器按鈕
    4.增加狀態列和快捷鍵
    5.增加音量條

--- kkbox/GUI.py
+++ kkbox/GUI.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5.QtMultimedia import *
 import sys
 sys.path.append('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/UI_Designer')
 from main_screen import Ui_MainWindow
@@ -16,11 +17,22 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+        # 播放器設置
+
+        # 播放器按鈕設置
+        # self.pushButton_play.clicked.connect(self.playMusic)
+        # self.pushButton_previous.clicked.connect(self.previousMusic)
+        # self.pushButton_next.clicked.connect(self.nextMusic)
+        # self.pushButton_random.clicked.connect(self.randomMusic)
+        # self.pushButton_repeat.clicked.connect(self.repeatMusic)
+
         # 狀態列功能設置
         self.retranslateUi(self)
         self.actionClose.triggered.connect(app.exit)
+
         # 取得comboBox_place, comboBox_release, comboBox_lang選取的值
         self.pushButton_OK.clicked.connect(self.get_comboBoxValue)
+
     # 地區設置
     def get_comboBoxValue(self):
         place_select_value = self.comboBox_place.currentText()
@@ -30,6 +42,9 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         print(place_select_value)
         print(release_select_value)
         print(lang_select_value)
+    # 播放器按鈕功能設置
+    # def playMusic(self):
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)

638a6719fdab7a889252e78a319bab1c3d6818c8
Glow <fnckaliezer@gmail.com>
2020-11-15 18:45:01 +0800

    更新播放器
    1.現在會顯示歌曲清單
    2.上一首, 下一首也正常使用
    3.歌曲進度條會動(Lag....)
    4.新增lyris跟playlist的按鍵
    5.playlist按鍵 按下去之後會隱藏歌曲清單
    
    Bug:
    1.進度條超lag
    2.雙擊歌曲清單中的歌曲 無法直接播放歌曲
    3.icon還是無法正確顯示
    4.random and repeat 功能無法作動

--- kkbox/GUI.py
+++ kkbox/GUI.py
@@ -1,13 +1,16 @@
+import time
+import sys
+sys.path.append('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/UI_Designer')
+import unittest
+
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtMultimedia import *
-import sys
-# sys.path.append('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/UI_Designer')
-# sys.path.append('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/UI_Designer/icon')
+from PyQt5.Qt import QUrl
 from main_screen import Ui_MainWindow
-import unittest
+
 
 
 class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
@@ -16,18 +19,42 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
-        # 播放器設置
+        # 位址設定
+        media_root = QFileInfo(__file__).absolutePath()
+        image_root = QFileInfo(__file__).absolutePath()
+
+        # icon設定
+        # Icons made by <ahref="https://www.flaticon.com/authors/bqlqn"title = "bqlqn">bqlqn</a>from<ahref = "https://www.flaticon.com/"title = "Flaticon">www.flaticon.com</a>
+        self.setWindowIcon(QIcon(image_root + 'UI_Designer/icon/kkbox_app_icon.png'))
+
+        # 播放器
         self.playlist = QMediaPlaylist(self)
         self.player = QMediaPlayer(self)
-        # 取得音樂檔案位置
-        self.media_content = QMediaContent(QUrl.fromLocalFile('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/text.mp3'))
-        # self.player.setMedia(QMediaContent(QUrl('http://example.com/music.mp3')))
-        self.player.setMedia(self.media_content)
+
+        ## 進度條設置
+        self.player.durationChanged.connect(self.get_duration_func)
+        self.player.positionChanged.connect(self.get_position_func)
         ## 音量設置
         self.volume_Slider.valueChanged.connect(self.volume_slider_func)
         self.player.setVolume(50)
+
+        ## 播放列表設置
+            # 取得音樂檔案位置
+            # self.media_content = QMediaContent(QUrl.fromLocalFile('/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/text.mp3'))
+            # self.player.setMedia(QMediaContent(QUrl('http://example.com/music.mp3')))
+            # self.player.setMedia(self.media_content)
+
+        self.player.setPlaylist(self.playlist)
+        self.media_list = ['/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/text.mp3',
+                           '/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/02.longing.mp3',
+                           '/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/03.crack.mp3',
+                           '/Users/glow/Desktop/IECS/Data_Science_and_GUI/group_demo/web_crawler/kkbox/05.glow.mp3']
+        for m in self.media_list:
+            self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(m)))
+        self.playlist.setPlaybackMode(QMediaPlaylist.Sequential)
+        self.playlist_listWidget.addItems([m.split('/')[-1] for m in self.media_list])
+        self.playlist.setCurrentIndex(self.playlist_listWidget.currentRow())
         self.player.play()
-        self.duration = self.player.duration() #取得歌曲長度(macOS無法設定...)
 
         # 狀態列功能設置
         self.retranslateUi(self)
@@ -42,19 +69,19 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         # self.actionVolume_Down.triggered.connect(self)
 
         # 播放器按鈕設置
-        # Icons made by <ahref="https://www.flaticon.com/authors/bqlqn"title = "bqlqn">bqlqn</a>from<ahref = "https://www.flaticon.com/"title = "Flaticon">www.flaticon.com</a>
-
         self.pushButton_play.clicked.connect(self.playMusic)
         self.pushButton_previous.clicked.connect(self.previousMusic)
         self.pushButton_next.clicked.connect(self.nextMusic)
         self.pushButton_random.clicked.connect(self.Music_mode)
         self.pushButton_repeat.clicked.connect(self.Music_mode)
+        # self.pushButton_lyris.clicked.connect(self.Music_mode)
+        self.pushButton_playlist.clicked.connect(self.playlist_setting)
 
         # 取得comboBox_place, comboBox_release, comboBox_lang選取的值
         self.pushButton_OK.clicked.connect(self.get_comboBoxValue)
 
     # def file_open(self):
-    #     # 開啟檔案
+    # 開啟檔案
     def playMusic(self):
         # 播放音樂
         if self.player.state() == 1:
@@ -75,6 +102,8 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
                 self.playlist.previous()
     def Music_mode(self):
         # 音樂模式
+        print(self.playlist.playbackMode)
+        
         if self.playlist.playbackMode() == 2:
             self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
 
@@ -83,6 +112,34 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
 
         elif self.playlist.playbackMode() == 4:
             self.playlist.setPlaybackMode(QMediaPlaylist.Sequential)
+
+    def playlist_setting(self):
+        # 播放列表
+        if self.playlist_listWidget.isHidden():
+            self.playlist_listWidget.show()
+        else:
+            self.playlist_listWidget.hide()
+
+    def get_duration_func(self, d_time):
+        # 取得進度條位址
+        self.player_progressBar.setRange(0, d_time)
+        self.player_progressBar.setEnabled(True)
+        self.get_time_func(d_time)
+
+    def get_time_func(self, d_time):
+        # 播放時間
+        seconds = int(d_time / 1000)
+        minutes = int(seconds / 60)
+        seconds -= minutes * 60
+        # if minutes == 0 and seconds == 0:
+        #     self.time_label.setText('--/--')
+        # else:
+        #     self.time_label.setText('{}:{}'.format(minutes, seconds))
+
+    def get_position_func(self, p):
+        # 音樂進度條
+        self.player_progressBar.setValue(p)
+
     def volume_slider_func(self, value):
         # 音量條
         self.player.setVolume(value)

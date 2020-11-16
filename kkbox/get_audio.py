452fca28074b28db11bd6a178e896399bc33198e
pu1027 <69178198+pu1027@users.noreply.github.com>
2020-11-16 13:06:44 +0800

    Update get_audio.py
    補充註解

--- kkbox/get_audio.py
+++ kkbox/get_audio.py
@@ -5,7 +5,7 @@ import time
 import re
 
 
-def validateTitle(title):
+def validateTitle(title): #替代特殊符號
     rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
     new_title = re.sub(rstr, "_", title)  # 替換為下劃線
     return new_title
@@ -17,36 +17,32 @@ def get_audio(SongType, Area, Lang, Cate, Date):
     options.add_argument('--headless')    
 
     browser = 'https://kma.kkbox.com/charts/daily/{}?cate={}&date={}&lang={}&terr={}'.format(SongType,Cate,Date,Lang,Area)
-    # driver = webdriver.Chrome(chrome_options=options, executable_path="./chromedriver.exe") # Use Chrome
-    driver = webdriver.Chrome(executable_path="./chromedriver.exe") # Use Chrome
+    # driver = webdriver.Chrome(chrome_options=options, executable_path="./chromedriver.exe") # 背景執行
+    driver = webdriver.Chrome(executable_path="./chromedriver.exe") 
     
-    # driver.maximize_window()
+    # driver.maximize_window() #全螢幕
     driver.get(browser)
     time.sleep(3)
     
         
-    # driver.find_element_by_link_text('日榜').click()
-    # time.sleep(5)
-    # driver.find_element_by_link_text(sn).click()
-    # time.sleep(5)
-    # driver.find_element_by_partial_link_text('全部試聽').click()
+
     for i in range(2,53,1):
-        if i == 32:
+        if i == 32: #li[32] 出現例外
             continue
         driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/ul/li[{}]/a/div/div[1]/span[1]/span[1]".format(i)).click()
         driver.implicitly_wait(5)
         driver.find_element_by_xpath("/html/body/div[5]/div[2]/div[2]/div[1]/div/button[1]").click()
         driver.implicitly_wait(5)
-        # soup = BeautifulSoup(driver.page_source, 'html.parser')
+        
             
-        audio=driver.find_element_by_tag_name('audio')
-        song_name=driver.find_element_by_xpath("/html/body/div[5]/div[2]/div[2]/div[1]/h1")
-        print(song_name.text)
+        audio=driver.find_element_by_tag_name('audio') #試聽檔案位置
+        song_name=driver.find_element_by_xpath("/html/body/div[5]/div[2]/div[2]/div[1]/h1") #歌詞位置
+        print(song_name.text) #下載歌詞 待補
         time.sleep(1)
         urllib.request.urlretrieve(audio.get_attribute('src'),"{}30S試聽.mp3".format(validateTitle(song_name.text)))
         time.sleep(1)
-        # print('done')
-        driver.back()
+        print('done')
+        driver.back()#上一頁
         
 
 

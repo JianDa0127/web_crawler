c5a0525cd5b26579e0bbd380dc640cf7a6173488
pu1027 <69178198+pu1027@users.noreply.github.com>
2020-11-16 11:59:16 +0800

    爬試聽檔案

---
+++ kkbox/get_audio.py
@@ -0,0 +1,53 @@
+import urllib.request
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import re
+
+
+def validateTitle(title):
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_title = re.sub(rstr, "_", title)  # 替換為下劃線
+    return new_title
+
+
+def get_audio(SongType, Area, Lang, Cate, Date):
+
+    options = webdriver.ChromeOptions()  #背景執行
+    options.add_argument('--headless')    
+
+    browser = 'https://kma.kkbox.com/charts/daily/{}?cate={}&date={}&lang={}&terr={}'.format(SongType,Cate,Date,Lang,Area)
+    # driver = webdriver.Chrome(chrome_options=options, executable_path="./chromedriver.exe") # Use Chrome
+    driver = webdriver.Chrome(executable_path="./chromedriver.exe") # Use Chrome
+    
+    # driver.maximize_window()
+    driver.get(browser)
+    time.sleep(3)
+    
+        
+    # driver.find_element_by_link_text('日榜').click()
+    # time.sleep(5)
+    # driver.find_element_by_link_text(sn).click()
+    # time.sleep(5)
+    # driver.find_element_by_partial_link_text('全部試聽').click()
+    for i in range(2,53,1):
+        if i == 32:
+            continue
+        driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/ul/li[{}]/a/div/div[1]/span[1]/span[1]".format(i)).click()
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath("/html/body/div[5]/div[2]/div[2]/div[1]/div/button[1]").click()
+        driver.implicitly_wait(5)
+        # soup = BeautifulSoup(driver.page_source, 'html.parser')
+            
+        audio=driver.find_element_by_tag_name('audio')
+        song_name=driver.find_element_by_xpath("/html/body/div[5]/div[2]/div[2]/div[1]/h1")
+        print(song_name.text)
+        time.sleep(1)
+        urllib.request.urlretrieve(audio.get_attribute('src'),"{}30S試聽.mp3".format(validateTitle(song_name.text)))
+        time.sleep(1)
+        # print('done')
+        driver.back()
+        
+
+
+get_audio('song','tw','tc','104','2020-11-15')

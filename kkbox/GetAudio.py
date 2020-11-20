5658c008ba3f65aa81f7fbe3433fbb75b64bbab7
pu1027 <69178198+pu1027@users.noreply.github.com>
2020-11-20 21:32:34 +0800

    歌詞爬蟲
    1.補上歌詞爬蟲
    2.爬蟲檔案分別放在audio和lyrics資料夾，沒有會自動新增

--- kkbox/GetAudio.py
+++ kkbox/GetAudio.py
@@ -1,17 +1,29 @@
 from urllib.request import urlretrieve
 from selenium import webdriver
 import time
+import os
 
 def get_audio(SongType, Area, Lang, Rank, Cate, Date):
     #背景執行
+    # url = 'https://kma.kkbox.com/charts/daily/{}?cate={}&date={}&lang={}&terr={}'.format(SongType,Cate,Date,Lang,Area)
     # options = webdriver.ChromeOptions() 
     # options.add_argument('--headless')    
-    # driver = webdriver.Chrome(chrome_options=options, executable_path="./chromedriver.exe") # 背景執行
+    # browser = webdriver.Chrome(chrome_options=options, executable_path="./chromedriver.exe") # 背景執行
+    # browser.get(url)
     
     url = 'https://kma.kkbox.com/charts/daily/{}?cate={}&date={}&lang={}&terr={}'.format(SongType,Cate,Date,Lang,Area)
     browser = webdriver.Chrome() 
     browser.get(url)
-    # time.sleep(3)
+    
+    #建立資料夾存放試聽檔
+    AudioPath = "audio"
+    if not os.path.isdir(AudioPath): #檢查資料夾是否存在
+        os.mkdir(AudioPath)
+    #建立資料夾存放歌詞
+    lyricsPath = "lyrics"
+    if not os.path.isdir(lyricsPath): #檢查資料夾是否存在
+        os.mkdir(lyricsPath)
+   
 
     Rank = int(Rank)+1 if int(Rank)>30 else int(Rank)
     for i in range(2,Rank+2):
@@ -19,9 +31,8 @@ def get_audio(SongType, Area, Lang, Rank, Cate, Date):
         #單曲頁面
         browser.find_element_by_xpath("/html/body/div[3]/div/div[2]/ul/li[{}]/a/div/div[1]/span[1]/span[1]".format(i)).click()
         browser.implicitly_wait(5)
-        #歌詞
-        lyrics = browser.find_element_by_xpath('/html/body/div[5]/div[2]/div[2]/div[1]/p').text
-        print(lyrics)
+       
+        
         #試聽檔案
         browser.find_element_by_xpath("/html/body/div[5]/div[2]/div[2]/div[1]/div/button[1]").click()
         browser.implicitly_wait(5)
@@ -30,18 +41,31 @@ def get_audio(SongType, Area, Lang, Rank, Cate, Date):
         #儲存試聽檔案
         song_name = browser.find_element_by_xpath('/html/body/div[5]/div[2]/div[2]/div[1]/h1').text
         song_name = song_name.split(' -')[0].split(' (')[0].replace('?','')
+        artist_name =browser.find_element_by_xpath('/html/body/div[5]/div[2]/div[1]/div[1]/div/dl/dd/a').text
+    
         try:
-            urlretrieve(audio.get_attribute('src'),"30s_{:02d}-{}.mp3".format(i-2 if i>32 else i-1,song_name))
+            urlretrieve(audio.get_attribute('src'), os.path.join(AudioPath, "30s-{}-{}.mp3".format(song_name, artist_name)))
+            # urlretrieve(audio.get_attribute('src'),"30s_{:02d}-{}.mp3".format(i-2 if i>32 else i-1,song_name))
+            time.sleep(1) #確保下載完整
             # print('[Success 30s download]',song_name)
         except:
-            # print('[Fail 30s download]',song_name)
+            print('[Fail 30s download]',song_name)
             pass
-        time.sleep(1)
+            
+        #儲存歌詞檔案
+        try:
+            lyrics = browser.find_element_by_xpath('/html/body/div[5]/div[2]/div[2]/div[1]/p').text
+            f = open( lyricsPath + "\\" +'{}-{}.txt'.format(song_name,artist_name), 'w',encoding="utf-8")
+            f.write(lyrics)
+            f.close()
+        except:
+            print('[Fail download]',song_name)
+            pass
+        
         browser.back() #上一頁
     #關瀏覽器
     browser.close()
-    #回傳歌詞(未實現的待改功能)
-    # return lyrics
+
 
 # 測試 get_audio(SongType, Area, Lang, Rank, Cate, Date)
-# get_audio('song','tw','tc','2','104','2020-11-15')
+get_audio('song','tw','tc','50','104','2020-11-15')
